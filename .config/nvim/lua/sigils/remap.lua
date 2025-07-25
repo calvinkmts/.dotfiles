21f13e3a707a17f9e22de56218281b84865ba73f
Calvin Kamtoso <calvinkmts@gmail.com>
2025-07-26 01:42:58 +0700

    updated nvim remap for easier copy/paste

--- .config/nvim/lua/sigils/remap.lua
+++ .config/nvim/lua/sigils/remap.lua
@@ -3,7 +3,19 @@ vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>fv", vim.cmd.Ex, { desc = 'Files View{:Ex}'})
 
+-- This code is to move selected contents up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Copying from buffer to system clipboard
+vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Y", '"+yg_', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>yy", '"+yy', { noremap = true, silent = true })
+
+-- Pasting from system clipboard to buffer 
+vim.keymap.set("v", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>P", '"+P', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>P", '"+P', { noremap = true, silent = true })
 
