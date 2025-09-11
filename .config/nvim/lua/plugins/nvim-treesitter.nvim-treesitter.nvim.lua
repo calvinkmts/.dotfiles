75fad4e7eea753c925f083d0e18b37f3d8508cd9
Calvin Kamtoso <calvinkmts@gmail.com>
2025-09-11 17:33:07 +0700

    feat(nvim): update treesitter config

--- .config/nvim/lua/plugins/nvim-treesitter.nvim-treesitter.nvim.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.nvim-treesitter.nvim.lua
@@ -2,5 +2,25 @@ return {
     'nvim-treesitter/nvim-treesitter',
     branch = 'master',
     lazy = false,
-    build = ':TSUpdate'
+    build = ':TSUpdate', 
+
+    config = function()
+        local configs = require('nvim-treesitter.configs')
+
+        configs.setup({
+            ensure_installed = {
+                'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline', 
+                'regex', 'bash', 'json', 'toml', 'yaml',
+                'python', 'javascript', 'typescript', 'tsx', 'sql', 'dockerfile'
+            },
+
+            sync_install = true,
+            auto_install = true,
+
+            highlight = {
+                enable = true, 
+                additional_vim_regex_highlighting = false,
+            }
+        })
+    end
 }
